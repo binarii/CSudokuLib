428676d870e57d7c2dde744f4462d70bd0cdfed2
Ryan Zuklie <rzuklie@gmail.com>
2015-10-03 23:58:43 -0400

    Implemented scramble solve for board generation

--- library/include/CSudokuLib/sudoku.h
+++ library/include/CSudokuLib/sudoku.h
@@ -2,9 +2,11 @@
 #define CSUDOKU_SUDOKU_H
 
 #include "prerequisites.h"
+#include "Stopwatch.h"
 #include "util.h"
 
 #include "AbstractBoard.h"
+#include "SolveMethod.h"
 #include "QuickSolve.h"
 
 #endif //CSUDOKU_SUDOKU_H
